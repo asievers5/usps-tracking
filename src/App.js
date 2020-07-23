07522722d4ca1cb564cd623adcf7f39afeb7b5d4
Andrew Sievers <andrew.sievers@selu.edu>
2020-07-22 20:27:18 -0500

    fixed bugs preventing pages to display

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import {
   BrowserRouter as Router,
   Switch,
 } from 'react-router-dom'
+import store from './redux/store'
+import AddressValidationPage from './components/pages/AddressValidation';
+import PriceEstimatorPage from './components/pages/PriceEstimator';
+import TrackingPage from './components/pages/Tracking/index';
+import HomePage from './components/pages/Home'
+import NavBar from './components/common/Nav/NavBar'
+import Footer from './components/common/Footer/Footer'
 
 const app = () => {
-  <Provider store={store}>
-      <Router>
-          <NavBar/><p>test</p>
-          <Switch>
-              <Route path="/price-estimator" component={PriceEstimatorPage} />
-              <Route path="/address-validation" component={AddressValidationPage} />
-              <Route path="/usps-tracking" component={TrackingPage} />
-              <Route path="/" exact component={HomePage} />
-          </Switch>
-          <Footer />
-      </Router>
-  </Provider>
+  return (
+          <Provider store={store}>
+            <Router>
+                <NavBar/><p>test</p>
+                <Switch>
+                    <Route path="/price-estimator" component={PriceEstimatorPage} />
+                    <Route path="/address-validation" component={AddressValidationPage} />
+                    <Route path="/usps-tracking" component={TrackingPage} />
+                    <Route path="/" exact component={HomePage} />
+                </Switch>
+                <Footer />
+            </Router>
+        </Provider>
+      )
 }
 
 export default app;
